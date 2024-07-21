0a5df8a69e439675b44cec594e2a574ad6043c87
Faizan Ahmad <faizanahmad2582001@gmail.com>
2024-07-21 15:23:23 +0500

    Renewal Screen Designed, only sumbit button function remains

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+
 import 'package:student_potal_aup/splash_screen.dart';
 
 void main() {
--- lib/select_degree_widget.dart
+++ lib/select_degree_widget.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+
 import 'package:flutter/cupertino.dart';
+
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class SelectDegreeWidget extends StatelessWidget {
